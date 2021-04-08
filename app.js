a4a3caab7ed16a798dce76a0278a57cffa3af533
Bruno Duarte <brunoduartedeoliveira@hotmail.com>
2021-04-08 15:39:27 -0300

    create routes

--- app.js
+++ app.js
@@ -1,20 +1,26 @@
 var express = require('express');
 var path = require('path');
-var cookieParser = require('cookie-parser');
+
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+
+var main_usersRouter = require('./app/routes/main_users');
+var prof_usersRouter = require('./app/routes/prof_users');
+var local_usersRouter = require('./app/routes/local_users');
+var service_classRouter = require('./app/routes/service_class');
 
 var app = express();
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
+
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+
+app.use('/main_users', main_usersRouter);
+app.use('/prof_users', prof_usersRouter);
+app.use('/local_users', local_usersRouter);
+app.use('/service_class', service_classRouter);
 
 module.exports = app;
