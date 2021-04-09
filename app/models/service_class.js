31b6ca09bacc71faa1cd417bd410bb925cc453a4
Bruno Duarte <brunoduartedeoliveira@hotmail.com>
2021-04-09 16:17:44 -0300

    inclede association professional to create service class

--- app/models/service_class.js
+++ app/models/service_class.js
@@ -3,14 +3,21 @@ const mongoose = require('mongoose');
 var serviceClassSchema = new mongoose.Schema({
     //INCLUIR TODAS AS PROPRIEDADES QUE UMA CLASSE/SERVIÇO TERÁ
     class_type: String,
-    isRemote: Boolean,
+    isRemote: { type: Boolean, default: true },
     created_at: { type: Date, default: Date.now },
     updated_at: { type: Date, default: Date.now },
+    class_date: {type: Date},
+    class_time: {type: Date},
     author: {
         type: mongoose.Schema.Types.ObjectId,
         ref: 'MainUser',
         required: true
-      }
+      },
+    professional: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'ProfUser',
+        required: true
+    }
   });
 
   module.exports = mongoose.model('ServiceClass', serviceClassSchema);
--- app/routes/service_class.js
+++ app/routes/service_class.js
@@ -3,9 +3,16 @@ var router = express.Router();
 const ServiceClass = require('../models/service_class.js');
 const withAuth = require('../middlewares/main_user_auth');
 
-router.post('/', withAuth, async (req, res) => {
-  const { class_type, isRemote, } = req.body;
-  var service_class = new ServiceClass({class_type: class_type, isRemote: isRemote, author: req.main_user._id});
+router.post('/:id', withAuth, async (req, res) => {
+  const { class_type, isRemote, class_date, class_time } = req.body;
+  const { id } = req.params;
+  var service_class = new ServiceClass({
+    class_type: class_type, 
+    isRemote: isRemote, 
+    class_date:class_date, 
+    class_time:class_time,
+    author: req.main_user._id, 
+    professional: id});
 
   try {
     await service_class.save();
