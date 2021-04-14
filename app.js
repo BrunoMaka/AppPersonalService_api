1e206ee17795a9e7cd40bb33695a96d9671ff5c2
Bruno Duarte <brunoduartedeoliveira@hotmail.com>
2021-04-14 13:43:55 -0300

    cors added

--- app.js
+++ app.js
@@ -2,8 +2,7 @@ var express = require('express');
 var path = require('path');
 var logger = require('morgan');
 require ('./config/database')
-
-
+var cors = require('cors')
 
 var main_usersRouter = require('./app/routes/main_users');
 var prof_usersRouter = require('./app/routes/prof_users');
@@ -15,9 +14,8 @@ var app = express();
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-
 app.use(express.static(path.join(__dirname, 'public')));
-
+app.use(cors())
 
 app.use('/main_users', main_usersRouter);
 app.use('/prof_users', prof_usersRouter);
